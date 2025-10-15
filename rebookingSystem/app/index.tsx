d41750c8d11aa0a0b1ee0f034a819e5c171e0def
Matthew Robert Willoughby <eduv4772354@vossie.net>
2025-10-15 19:57:22 +0200

    Im fed up with this

--- rebookingSystem/app/index.tsx
+++ rebookingSystem/app/index.tsx
@@ -2,5 +2,5 @@ import { Redirect } from "expo-router";
 
 export default function Index() {
   // Always redirect to login page
-  return <Redirect href="/auth/auth-login" />;
+  return <Redirect href="./auth/auth-login" />;
 }
